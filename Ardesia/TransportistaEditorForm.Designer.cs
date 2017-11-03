f2d8f3dcd20c00cdb7cf5c3f789ea0e6012acd8f
Facundo <facuk23@gmail.com>
2017-11-02 21:46:55 -0300

    transportistaEditor

---
+++ Ardesia/TransportistaEditorForm.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace Ardesia
+{
+    partial class TransportistaEditorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TransportistaEditorForm));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.idLabel = new System.Windows.Forms.Label();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.nombreLabel = new System.Windows.Forms.Label();
+            this.nombreTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.apellidoTextBox = new System.Windows.Forms.TextBox();
+            this.telefonoLabel = new System.Windows.Forms.Label();
+            this.ciudadComboBox = new System.Windows.Forms.ComboBox();
+            this.ciudadLabel = new System.Windows.Forms.Label();
+            this.provinciaComboBox = new System.Windows.Forms.ComboBox();
+            this.provinciaLlabel = new System.Windows.Forms.Label();
+            this.paisComboBox = new System.Windows.Forms.ComboBox();
+            this.paisLabel = new System.Windows.Forms.Label();
+            this.calleComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cpLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.exitSimpleButton = new DevExpress.XtraEditors.SimpleButton();
+            this.saveSimpleButton = new DevExpress.XtraEditors.SimpleButton();
+            this.telefonoTextBox = new System.Windows.Forms.TextBox();
+            this.dominioLabel = new System.Windows.Forms.Label();
+            this.camionLabel = new System.Windows.Forms.Label();
+            this.dominioCamionTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "disk.png");
+            this.imageList1.Images.SetKeyName(1, "door--arrow.png");
+            // 
+            // idLabel
+            // 
+            this.idLabel.AutoSize = true;
+            this.idLabel.Location = new System.Drawing.Point(14, 11);
+            this.idLabel.Name = "idLabel";
+            this.idLabel.Size = new System.Drawing.Size(27, 17);
+            this.idLabel.TabIndex = 3;
+            this.idLabel.Text = "ID:";
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(59, 7);
+            this.idTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(151, 23);
+            this.idTextBox.TabIndex = 4;
+            // 
+            // nombreLabel
+            // 
+            this.nombreLabel.AutoSize = true;
+            this.nombreLabel.Location = new System.Drawing.Point(218, 11);
+            this.nombreLabel.Name = "nombreLabel";
+            this.nombreLabel.Size = new System.Drawing.Size(62, 17);
+            this.nombreLabel.TabIndex = 3;
+            this.nombreLabel.Text = "Nombre:";
+            // 
+            // nombreTextBox
+            // 
+            this.nombreTextBox.Location = new System.Drawing.Point(280, 7);
+            this.nombreTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.nombreTextBox.Name = "nombreTextBox";
+            this.nombreTextBox.Size = new System.Drawing.Size(151, 23);
+            this.nombreTextBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(439, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Apellido:";
+            // 
+            // apellidoTextBox
+            // 
+            this.apellidoTextBox.Location = new System.Drawing.Point(509, 7);
+            this.apellidoTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.apellidoTextBox.Name = "apellidoTextBox";
+            this.apellidoTextBox.Size = new System.Drawing.Size(147, 23);
+            this.apellidoTextBox.TabIndex = 4;
+            // 
+            // telefonoLabel
+            // 
+            this.telefonoLabel.AutoSize = true;
+            this.telefonoLabel.Location = new System.Drawing.Point(663, 11);
+            this.telefonoLabel.Name = "telefonoLabel";
+            this.telefonoLabel.Size = new System.Drawing.Size(30, 17);
+            this.telefonoLabel.TabIndex = 3;
+            this.telefonoLabel.Text = "Tel:";
+            // 
+            // ciudadComboBox
+            // 
+            this.ciudadComboBox.FormattingEnabled = true;
+            this.ciudadComboBox.Location = new System.Drawing.Point(280, 39);
+            this.ciudadComboBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.ciudadComboBox.Name = "ciudadComboBox";
+            this.ciudadComboBox.Size = new System.Drawing.Size(151, 24);
+            this.ciudadComboBox.TabIndex = 7;
+            // 
+            // ciudadLabel
+            // 
+            this.ciudadLabel.AutoSize = true;
+            this.ciudadLabel.Location = new System.Drawing.Point(218, 44);
+            this.ciudadLabel.Name = "ciudadLabel";
+            this.ciudadLabel.Size = new System.Drawing.Size(55, 17);
+            this.ciudadLabel.TabIndex = 6;
+            this.ciudadLabel.Text = "Ciudad:";
+            // 
+            // provinciaComboBox
+            // 
+            this.provinciaComboBox.FormattingEnabled = true;
+            this.provinciaComboBox.Location = new System.Drawing.Point(509, 39);
+            this.provinciaComboBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.provinciaComboBox.Name = "provinciaComboBox";
+            this.provinciaComboBox.Size = new System.Drawing.Size(147, 24);
+            this.provinciaComboBox.TabIndex = 9;
+            // 
+            // provinciaLlabel
+            // 
+            this.provinciaLlabel.AutoSize = true;
+            this.provinciaLlabel.Location = new System.Drawing.Point(439, 44);
+            this.provinciaLlabel.Name = "provinciaLlabel";
+            this.provinciaLlabel.Size = new System.Drawing.Size(68, 17);
+            this.provinciaLlabel.TabIndex = 8;
+            this.provinciaLlabel.Text = "Provincia:";
+            // 
+            // paisComboBox
+            // 
+            this.paisComboBox.FormattingEnabled = true;
+            this.paisComboBox.Location = new System.Drawing.Point(706, 39);
+            this.paisComboBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.paisComboBox.Name = "paisComboBox";
+            this.paisComboBox.Size = new System.Drawing.Size(147, 24);
+            this.paisComboBox.TabIndex = 11;
+            // 
+            // paisLabel
+            // 
+            this.paisLabel.AutoSize = true;
+            this.paisLabel.Location = new System.Drawing.Point(664, 43);
+            this.paisLabel.Name = "paisLabel";
+            this.paisLabel.Size = new System.Drawing.Size(36, 17);
+            this.paisLabel.TabIndex = 10;
+            this.paisLabel.Text = "Pais:";
+            // 
+            // calleComboBox
+            // 
+            this.calleComboBox.FormattingEnabled = true;
+            this.calleComboBox.Location = new System.Drawing.Point(59, 74);
+            this.calleComboBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.calleComboBox.Name = "calleComboBox";
+            this.calleComboBox.Size = new System.Drawing.Size(371, 24);
+            this.calleComboBox.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Calle:";
+            // 
+            // cpLabel
+            // 
+            this.cpLabel.AutoSize = true;
+            this.cpLabel.Location = new System.Drawing.Point(14, 44);
+            this.cpLabel.Name = "cpLabel";
+            this.cpLabel.Size = new System.Drawing.Size(30, 17);
+            this.cpLabel.TabIndex = 14;
+            this.cpLabel.Text = "CP:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 39);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(151, 23);
+            this.textBox1.TabIndex = 15;
+            // 
+            // exitSimpleButton
+            // 
+            this.exitSimpleButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.exitSimpleButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.exitSimpleButton.ImageOptions.ImageIndex = 1;
+            this.exitSimpleButton.ImageOptions.ImageList = this.imageList1;
+            this.exitSimpleButton.Location = new System.Drawing.Point(740, 126);
+            this.exitSimpleButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.exitSimpleButton.Name = "exitSimpleButton";
+            this.exitSimpleButton.Size = new System.Drawing.Size(114, 27);
+            this.exitSimpleButton.TabIndex = 17;
+            this.exitSimpleButton.Text = "&Salir";
+            // 
+            // saveSimpleButton
+            // 
+            this.saveSimpleButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveSimpleButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.saveSimpleButton.ImageOptions.ImageIndex = 0;
+            this.saveSimpleButton.ImageOptions.ImageList = this.imageList1;
+            this.saveSimpleButton.Location = new System.Drawing.Point(615, 126);
+            this.saveSimpleButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.saveSimpleButton.Name = "saveSimpleButton";
+            this.saveSimpleButton.Size = new System.Drawing.Size(118, 27);
+            this.saveSimpleButton.TabIndex = 16;
+            this.saveSimpleButton.Text = "&Guardar";
+            // 
+            // telefonoTextBox
+            // 
+            this.telefonoTextBox.Location = new System.Drawing.Point(706, 7);
+            this.telefonoTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.telefonoTextBox.Name = "telefonoTextBox";
+            this.telefonoTextBox.Size = new System.Drawing.Size(147, 23);
+            this.telefonoTextBox.TabIndex = 18;
+            // 
+            // dominioLabel
+            // 
+            this.dominioLabel.AutoSize = true;
+            this.dominioLabel.Location = new System.Drawing.Point(439, 73);
+            this.dominioLabel.Name = "dominioLabel";
+            this.dominioLabel.Size = new System.Drawing.Size(58, 17);
+            this.dominioLabel.TabIndex = 19;
+            this.dominioLabel.Text = "Dominio";
+            // 
+            // camionLabel
+            // 
+            this.camionLabel.AutoSize = true;
+            this.camionLabel.Location = new System.Drawing.Point(439, 90);
+            this.camionLabel.Name = "camionLabel";
+            this.camionLabel.Size = new System.Drawing.Size(57, 17);
+            this.camionLabel.TabIndex = 20;
+            this.camionLabel.Text = "camión:";
+            // 
+            // dominioCamionTextBox
+            // 
+            this.dominioCamionTextBox.Location = new System.Drawing.Point(509, 76);
+            this.dominioCamionTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dominioCamionTextBox.Name = "dominioCamionTextBox";
+            this.dominioCamionTextBox.Size = new System.Drawing.Size(147, 23);
+            this.dominioCamionTextBox.TabIndex = 21;
+            // 
+            // TransportistaEditorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 167);
+            this.Controls.Add(this.dominioCamionTextBox);
+            this.Controls.Add(this.camionLabel);
+            this.Controls.Add(this.dominioLabel);
+            this.Controls.Add(this.telefonoTextBox);
+            this.Controls.Add(this.exitSimpleButton);
+            this.Controls.Add(this.saveSimpleButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cpLabel);
+            this.Controls.Add(this.calleComboBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.paisComboBox);
+            this.Controls.Add(this.paisLabel);
+            this.Controls.Add(this.provinciaComboBox);
+            this.Controls.Add(this.provinciaLlabel);
+            this.Controls.Add(this.ciudadComboBox);
+            this.Controls.Add(this.ciudadLabel);
+            this.Controls.Add(this.apellidoTextBox);
+            this.Controls.Add(this.telefonoLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nombreTextBox);
+            this.Controls.Add(this.nombreLabel);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.idLabel);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "TransportistaEditorForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Empleado";
+            this.Load += new System.EventHandler(this.TransportistaEditorForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label idLabel;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.Label nombreLabel;
+        private System.Windows.Forms.TextBox nombreTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox apellidoTextBox;
+        private System.Windows.Forms.Label telefonoLabel;
+        private System.Windows.Forms.ComboBox ciudadComboBox;
+        private System.Windows.Forms.Label ciudadLabel;
+        private System.Windows.Forms.ComboBox provinciaComboBox;
+        private System.Windows.Forms.Label provinciaLlabel;
+        private System.Windows.Forms.ComboBox paisComboBox;
+        private System.Windows.Forms.Label paisLabel;
+        private System.Windows.Forms.ComboBox calleComboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label cpLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ImageList imageList1;
+        private DevExpress.XtraEditors.SimpleButton exitSimpleButton;
+        private DevExpress.XtraEditors.SimpleButton saveSimpleButton;
+        private System.Windows.Forms.TextBox telefonoTextBox;
+        private System.Windows.Forms.Label dominioLabel;
+        private System.Windows.Forms.Label camionLabel;
+        private System.Windows.Forms.TextBox dominioCamionTextBox;
+    }
+}
---
+++ Ardesia/TransportistaEditorForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ardesia
+{
+    public partial class TransportistaEditorForm : DevExpress.XtraEditors.XtraForm
+    {
+        public TransportistaEditorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TransportistaEditorForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
