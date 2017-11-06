c5acdcce741c4de75e923ad0cc90c306b52bc5d7
Facundo <facuk23@gmail.com>
2017-11-05 21:36:05 -0300

    Insumo

---
+++ Ardesia/InsumoEditorForm.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Ardesia
+{
+    partial class InsumoEditorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InsumoEditorForm));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.idLabel = new System.Windows.Forms.Label();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.nombreLabel = new System.Windows.Forms.Label();
+            this.nombreTextBox = new System.Windows.Forms.TextBox();
+            this.proveedorComboBox = new System.Windows.Forms.ComboBox();
+            this.proveedorLabel = new System.Windows.Forms.Label();
+            this.sectorComboBox = new System.Windows.Forms.ComboBox();
+            this.sectorLlabel = new System.Windows.Forms.Label();
+            this.unidadMedidaComboBox = new System.Windows.Forms.ComboBox();
+            this.unidadMedidaLabel = new System.Windows.Forms.Label();
+            this.exitSimpleButton = new DevExpress.XtraEditors.SimpleButton();
+            this.saveSimpleButton = new DevExpress.XtraEditors.SimpleButton();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "disk.png");
+            this.imageList1.Images.SetKeyName(1, "door--arrow.png");
+            // 
+            // idLabel
+            // 
+            this.idLabel.AutoSize = true;
+            this.idLabel.Location = new System.Drawing.Point(14, 11);
+            this.idLabel.Name = "idLabel";
+            this.idLabel.Size = new System.Drawing.Size(27, 17);
+            this.idLabel.TabIndex = 3;
+            this.idLabel.Text = "ID:";
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(95, 8);
+            this.idTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(151, 23);
+            this.idTextBox.TabIndex = 4;
+            // 
+            // nombreLabel
+            // 
+            this.nombreLabel.AutoSize = true;
+            this.nombreLabel.Location = new System.Drawing.Point(252, 11);
+            this.nombreLabel.Name = "nombreLabel";
+            this.nombreLabel.Size = new System.Drawing.Size(62, 17);
+            this.nombreLabel.TabIndex = 3;
+            this.nombreLabel.Text = "Nombre:";
+            // 
+            // nombreTextBox
+            // 
+            this.nombreTextBox.Location = new System.Drawing.Point(320, 8);
+            this.nombreTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.nombreTextBox.Name = "nombreTextBox";
+            this.nombreTextBox.Size = new System.Drawing.Size(151, 23);
+            this.nombreTextBox.TabIndex = 4;
+            // 
+            // proveedorComboBox
+            // 
+            this.proveedorComboBox.FormattingEnabled = true;
+            this.proveedorComboBox.Location = new System.Drawing.Point(95, 39);
+            this.proveedorComboBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.proveedorComboBox.Name = "proveedorComboBox";
+            this.proveedorComboBox.Size = new System.Drawing.Size(151, 24);
+            this.proveedorComboBox.TabIndex = 7;
+            // 
+            // proveedorLabel
+            // 
+            this.proveedorLabel.AutoSize = true;
+            this.proveedorLabel.Location = new System.Drawing.Point(14, 43);
+            this.proveedorLabel.Name = "proveedorLabel";
+            this.proveedorLabel.Size = new System.Drawing.Size(77, 17);
+            this.proveedorLabel.TabIndex = 6;
+            this.proveedorLabel.Text = "Proveedor:";
+            // 
+            // sectorComboBox
+            // 
+            this.sectorComboBox.FormattingEnabled = true;
+            this.sectorComboBox.Location = new System.Drawing.Point(320, 39);
+            this.sectorComboBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.sectorComboBox.Name = "sectorComboBox";
+            this.sectorComboBox.Size = new System.Drawing.Size(151, 24);
+            this.sectorComboBox.TabIndex = 9;
+            // 
+            // sectorLlabel
+            // 
+            this.sectorLlabel.AutoSize = true;
+            this.sectorLlabel.Location = new System.Drawing.Point(252, 43);
+            this.sectorLlabel.Name = "sectorLlabel";
+            this.sectorLlabel.Size = new System.Drawing.Size(53, 17);
+            this.sectorLlabel.TabIndex = 8;
+            this.sectorLlabel.Text = "Sector:";
+            // 
+            // unidadMedidaComboBox
+            // 
+            this.unidadMedidaComboBox.FormattingEnabled = true;
+            this.unidadMedidaComboBox.Location = new System.Drawing.Point(93, 71);
+            this.unidadMedidaComboBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.unidadMedidaComboBox.Name = "unidadMedidaComboBox";
+            this.unidadMedidaComboBox.Size = new System.Drawing.Size(153, 24);
+            this.unidadMedidaComboBox.TabIndex = 11;
+            // 
+            // unidadMedidaLabel
+            // 
+            this.unidadMedidaLabel.AutoSize = true;
+            this.unidadMedidaLabel.Location = new System.Drawing.Point(14, 71);
+            this.unidadMedidaLabel.Name = "unidadMedidaLabel";
+            this.unidadMedidaLabel.Size = new System.Drawing.Size(73, 34);
+            this.unidadMedidaLabel.TabIndex = 10;
+            this.unidadMedidaLabel.Text = "Unidad de \r\nmedida:";
+            // 
+            // exitSimpleButton
+            // 
+            this.exitSimpleButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.exitSimpleButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.exitSimpleButton.ImageOptions.ImageIndex = 1;
+            this.exitSimpleButton.ImageOptions.ImageList = this.imageList1;
+            this.exitSimpleButton.Location = new System.Drawing.Point(361, 110);
+            this.exitSimpleButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.exitSimpleButton.Name = "exitSimpleButton";
+            this.exitSimpleButton.Size = new System.Drawing.Size(114, 27);
+            this.exitSimpleButton.TabIndex = 17;
+            this.exitSimpleButton.Text = "&Salir";
+            // 
+            // saveSimpleButton
+            // 
+            this.saveSimpleButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveSimpleButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.saveSimpleButton.ImageOptions.ImageIndex = 0;
+            this.saveSimpleButton.ImageOptions.ImageList = this.imageList1;
+            this.saveSimpleButton.Location = new System.Drawing.Point(236, 110);
+            this.saveSimpleButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.saveSimpleButton.Name = "saveSimpleButton";
+            this.saveSimpleButton.Size = new System.Drawing.Size(118, 27);
+            this.saveSimpleButton.TabIndex = 16;
+            this.saveSimpleButton.Text = "&Guardar";
+            // 
+            // InsumoEditorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 151);
+            this.Controls.Add(this.exitSimpleButton);
+            this.Controls.Add(this.saveSimpleButton);
+            this.Controls.Add(this.unidadMedidaComboBox);
+            this.Controls.Add(this.unidadMedidaLabel);
+            this.Controls.Add(this.sectorComboBox);
+            this.Controls.Add(this.sectorLlabel);
+            this.Controls.Add(this.proveedorComboBox);
+            this.Controls.Add(this.proveedorLabel);
+            this.Controls.Add(this.nombreTextBox);
+            this.Controls.Add(this.nombreLabel);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.idLabel);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "InsumoEditorForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Insumo";
+            this.Load += new System.EventHandler(this.InsumoEditorForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label idLabel;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.Label nombreLabel;
+        private System.Windows.Forms.TextBox nombreTextBox;
+        private System.Windows.Forms.ComboBox proveedorComboBox;
+        private System.Windows.Forms.Label proveedorLabel;
+        private System.Windows.Forms.ComboBox sectorComboBox;
+        private System.Windows.Forms.Label sectorLlabel;
+        private System.Windows.Forms.ComboBox unidadMedidaComboBox;
+        private System.Windows.Forms.Label unidadMedidaLabel;
+        private System.Windows.Forms.ImageList imageList1;
+        private DevExpress.XtraEditors.SimpleButton exitSimpleButton;
+        private DevExpress.XtraEditors.SimpleButton saveSimpleButton;
+    }
+}
---
+++ Ardesia/InsumoListForm.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Ardesia
+{
+    partial class InsumoListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InsumoListForm));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.deleteSimpleButton = new DevExpress.XtraEditors.SimpleButton();
+            this.editSimpleButton = new DevExpress.XtraEditors.SimpleButton();
+            this.addSimpleButton = new DevExpress.XtraEditors.SimpleButton();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.insumoGridView = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.gridColumnId = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.repositoryItemLookUpEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemLookUpEdit();
+            this.repositoryItemLookUpEdit3 = new DevExpress.XtraEditors.Repository.RepositoryItemLookUpEdit();
+            this.repositoryItemLookUpEdit2 = new DevExpress.XtraEditors.Repository.RepositoryItemLookUpEdit();
+            this.gridColumnProvedor = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumnSector = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumnNombre = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumnUnidadMedida = new DevExpress.XtraGrid.Columns.GridColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.insumoGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemLookUpEdit1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemLookUpEdit3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemLookUpEdit2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "plus-circle.png");
+            this.imageList1.Images.SetKeyName(1, "cross-circle.png");
+            this.imageList1.Images.SetKeyName(2, "pencil.png");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Filtro:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 12);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(237, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // deleteSimpleButton
+            // 
+            this.deleteSimpleButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.deleteSimpleButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.deleteSimpleButton.ImageOptions.ImageIndex = 1;
+            this.deleteSimpleButton.ImageOptions.ImageList = this.imageList1;
+            this.deleteSimpleButton.Location = new System.Drawing.Point(772, 267);
+            this.deleteSimpleButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.deleteSimpleButton.Name = "deleteSimpleButton";
+            this.deleteSimpleButton.Size = new System.Drawing.Size(118, 27);
+            this.deleteSimpleButton.TabIndex = 58;
+            this.deleteSimpleButton.Text = "&Borrar";
+            // 
+            // editSimpleButton
+            // 
+            this.editSimpleButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.editSimpleButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.editSimpleButton.ImageOptions.ImageIndex = 2;
+            this.editSimpleButton.ImageOptions.ImageList = this.imageList1;
+            this.editSimpleButton.Location = new System.Drawing.Point(651, 267);
+            this.editSimpleButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.editSimpleButton.Name = "editSimpleButton";
+            this.editSimpleButton.Size = new System.Drawing.Size(114, 27);
+            this.editSimpleButton.TabIndex = 57;
+            this.editSimpleButton.Text = "&Editar";
+            // 
+            // addSimpleButton
+            // 
+            this.addSimpleButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.addSimpleButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.addSimpleButton.ImageOptions.ImageIndex = 0;
+            this.addSimpleButton.ImageOptions.ImageList = this.imageList1;
+            this.addSimpleButton.Location = new System.Drawing.Point(526, 267);
+            this.addSimpleButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.addSimpleButton.Name = "addSimpleButton";
+            this.addSimpleButton.Size = new System.Drawing.Size(118, 27);
+            this.addSimpleButton.TabIndex = 56;
+            this.addSimpleButton.Text = "&Agregar";
+            // 
+            // gridView1
+            // 
+            this.gridView1.Name = "gridView1";
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gridControl1.EmbeddedNavigator.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.gridControl1.Location = new System.Drawing.Point(18, 63);
+            this.gridControl1.MainView = this.insumoGridView;
+            this.gridControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
+            this.repositoryItemLookUpEdit1,
+            this.repositoryItemLookUpEdit2,
+            this.repositoryItemLookUpEdit3});
+            this.gridControl1.Size = new System.Drawing.Size(860, 196);
+            this.gridControl1.TabIndex = 59;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.insumoGridView});
+            // 
+            // insumoGridView
+            // 
+            this.insumoGridView.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.gridColumnId,
+            this.gridColumnNombre,
+            this.gridColumnProvedor,
+            this.gridColumnSector,
+            this.gridColumnUnidadMedida});
+            this.insumoGridView.GridControl = this.gridControl1;
+            this.insumoGridView.Name = "insumoGridView";
+            this.insumoGridView.OptionsFilter.ShowAllTableValuesInFilterPopup = true;
+            this.insumoGridView.OptionsPrint.EnableAppearanceOddRow = true;
+            this.insumoGridView.OptionsPrint.PrintFilterInfo = true;
+            this.insumoGridView.OptionsView.HeaderFilterButtonShowMode = DevExpress.XtraEditors.Controls.FilterButtonShowMode.Button;
+            this.insumoGridView.OptionsView.ShowAutoFilterRow = true;
+            this.insumoGridView.OptionsView.ShowFooter = true;
+            this.insumoGridView.OptionsView.ShowGroupPanel = false;
+            // 
+            // gridColumnId
+            // 
+            this.gridColumnId.Caption = "Id";
+            this.gridColumnId.Name = "gridColumnId";
+            this.gridColumnId.Visible = true;
+            this.gridColumnId.VisibleIndex = 0;
+            // 
+            // repositoryItemLookUpEdit1
+            // 
+            this.repositoryItemLookUpEdit1.AutoHeight = false;
+            this.repositoryItemLookUpEdit1.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.repositoryItemLookUpEdit1.Name = "repositoryItemLookUpEdit1";
+            // 
+            // repositoryItemLookUpEdit3
+            // 
+            this.repositoryItemLookUpEdit3.AutoHeight = false;
+            this.repositoryItemLookUpEdit3.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.repositoryItemLookUpEdit3.Name = "repositoryItemLookUpEdit3";
+            // 
+            // repositoryItemLookUpEdit2
+            // 
+            this.repositoryItemLookUpEdit2.AutoHeight = false;
+            this.repositoryItemLookUpEdit2.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.repositoryItemLookUpEdit2.Name = "repositoryItemLookUpEdit2";
+            // 
+            // gridColumnProvedor
+            // 
+            this.gridColumnProvedor.Caption = "Proveedor";
+            this.gridColumnProvedor.Name = "gridColumnProvedor";
+            this.gridColumnProvedor.Visible = true;
+            this.gridColumnProvedor.VisibleIndex = 1;
+            // 
+            // gridColumnSector
+            // 
+            this.gridColumnSector.Caption = "Sector";
+            this.gridColumnSector.Name = "gridColumnSector";
+            this.gridColumnSector.Visible = true;
+            this.gridColumnSector.VisibleIndex = 2;
+            // 
+            // gridColumnNombre
+            // 
+            this.gridColumnNombre.Caption = "Nombre";
+            this.gridColumnNombre.Name = "gridColumnNombre";
+            this.gridColumnNombre.Visible = true;
+            this.gridColumnNombre.VisibleIndex = 3;
+            // 
+            // gridColumnUnidadMedida
+            // 
+            this.gridColumnUnidadMedida.Caption = "Unidad de medida";
+            this.gridColumnUnidadMedida.Name = "gridColumnUnidadMedida";
+            this.gridColumnUnidadMedida.Visible = true;
+            this.gridColumnUnidadMedida.VisibleIndex = 4;
+            // 
+            // InsumoListForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 309);
+            this.Controls.Add(this.gridControl1);
+            this.Controls.Add(this.deleteSimpleButton);
+            this.Controls.Add(this.editSimpleButton);
+            this.Controls.Add(this.addSimpleButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "InsumoListForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Insumos";
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.insumoGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemLookUpEdit1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemLookUpEdit3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemLookUpEdit2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ImageList imageList1;
+        private DevExpress.XtraEditors.SimpleButton deleteSimpleButton;
+        private DevExpress.XtraEditors.SimpleButton editSimpleButton;
+        private DevExpress.XtraEditors.SimpleButton addSimpleButton;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView insumoGridView;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumnId;
+        private DevExpress.XtraEditors.Repository.RepositoryItemLookUpEdit repositoryItemLookUpEdit1;
+        private DevExpress.XtraEditors.Repository.RepositoryItemLookUpEdit repositoryItemLookUpEdit3;
+        private DevExpress.XtraEditors.Repository.RepositoryItemLookUpEdit repositoryItemLookUpEdit2;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumnNombre;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumnProvedor;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumnSector;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumnUnidadMedida;
+    }
+}
